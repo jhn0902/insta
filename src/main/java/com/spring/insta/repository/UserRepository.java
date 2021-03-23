87ec9408dbe691f267215ba0277c000e92550b59
jihun <jhn0902@gmail.com>
2021-03-23 13:22:00 +0900

    .

--- src/main/java/com/spring/insta/repository/UserRepository.java
+++ src/main/java/com/spring/insta/repository/UserRepository.java
@@ -1,11 +1,9 @@
 package com.spring.insta.repository;
 
 import com.spring.insta.model.User;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import sun.jvm.hotspot.debugger.Page;
 
 import java.util.List;
 
